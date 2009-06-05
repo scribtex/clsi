30d014b4a7a31ea0ff2653ca42de5b20824dabfd
James <J.P.Allen-2@sms.ed.ac.uk>
2009-06-05 14:56:58 +0100

    Resources can now fetch content from URLs albeit in a fragile way

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 class Resource
   attr_reader :path, :modified_date, :url
 
@@ -9,18 +12,32 @@ class Resource
     @project = project
   end
 
+  # Return the content of this resource. This might come from being passed
+  # directly, from the cache or from an URL.
   def content
     if @content.nil?
-      raise NotImplementedError, 'cannot get content from URL'
+      content_from_url
     else
       @content
     end
   end
 
+  # Fetch the content from the URL of this resource
+  def content_from_url
+    # TODO: This could be made more robust with return value checking,
+    # redirect following, specific error catching etc.
+    Net::HTTP.get URI.parse(@url)
+  rescue
+    ""
+  end
+
+  # Return the path where this resource should be written to for compiling
   def path_to_file_on_disk
     File.join(LATEX_COMPILE_DIR, @project.unique_id, @path)
   end
 
+  # Write the contents of this resource to the location provided by the path
+  # of the resource
   def write_to_disk
     dir_path = File.dirname(path_to_file_on_disk)
     FileUtils.mkdir_p(dir_path)
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -4,19 +4,45 @@ describe Resource do
   before(:each) do
     @user = User.create!
     @project = Project.create!(:name => 'Test Project', :user => @user)
-    @resource = Resource.new(
-       'chapters/main.tex',
-       nil,
-       'Test content',
-       nil,
-       @project
-    )
   end
 
-  it "should write the file to disk in the compile directroy" do
-    @resource.write_to_disk
-    file_path = File.join(LATEX_COMPILE_DIR, @project.unique_id, 'chapters/main.tex')
-    File.exist?(file_path).should be_true
-    File.read(file_path).should eql 'Test content'
+  describe "with content passed directly" do
+    before(:each) do
+      @resource = Resource.new(
+         'chapters/main.tex',
+         nil,
+         'Test content',
+         nil,
+         @project
+      )
+    end
+
+    it "should write the file to disk in the compile directroy" do
+      @resource.write_to_disk
+      file_path = File.join(LATEX_COMPILE_DIR, @project.unique_id, 'chapters/main.tex')
+      File.exist?(file_path).should be_true
+      File.read(file_path).should eql 'Test content'
+    end
+
+    it "should return the content passed directly" do
+      @resource.content.should eql 'Test content'
+    end
+  end
+
+  describe "with content take from an URL" do
+    before(:each) do
+      @resource = Resource.new(
+         'chapters/main.tex',
+         nil,
+         nil,
+         'http://www.example.com/main.tex',
+         @project
+      )
+    end
+    
+    it "should return the content from the URL" do
+      Net::HTTP.should_receive(:get).with(URI.parse('http://www.example.com/main.tex')).and_return('URL content')
+      @resource.content.should eql 'URL content'
+    end
   end
 end
