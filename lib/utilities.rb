9be060953086747a681e527d7c48e8686a28334b
James Allen <jamesallen0108@gmail.com>
2010-01-15 21:30:56 +0000

    Improved URL grabbing by using wget instead

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require 'systemu'
 require 'uri'
 
 def generate_unique_string
@@ -21,28 +21,7 @@ end
 
 module Utilities
   def self.get_content_from_url(url)
-    # TODO: This could be made more robust with return value checking,
-    # redirect following, specific error catching etc.
-    def self.get_response_from_url(url)
-      url = URI.parse(url)
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        url.path = '/' if url.path.blank?
-        http.get(url.path)
-      }
-      return res
-    end
-
-    res = self.get_response_from_url(url)
-    if res.header['location']
-      res = get_response_from_url(res.header['location'])
-    end
-
-    if res.is_a?(Net::HTTPSuccess)
-      return res.body
-    else
-      return false
-    end
-  rescue SocketError
-    false
+    status, stdout, stdin = systemu(['wget', '-O', '-', url])
+    return stdout
   end
 end
