f2883dbce863d6f374e69ef521e0333a681f7298
James Allen <jamesallen0108@gmail.com>
2010-04-26 16:57:04 +0000

    Fixed DB mgiration to give mediumblob, not longblob

--- db/migrate/20100426155335_increase_url_cache_content_size.rb
+++ db/migrate/20100426155335_increase_url_cache_content_size.rb
@@ -1,6 +1,6 @@
 class IncreaseUrlCacheContentSize < ActiveRecord::Migration
   def self.up
-    change_column :url_caches, :content, :binary, :limit => 16.megabytes
+    change_column :url_caches, :content, :binary, :limit => 10.megabytes
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 
 ActiveRecord::Schema.define(:version => 20100426155335) do
 
-  create_table "projects", :force => true do |t|
-    t.string   "name"
-    t.string   "unique_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "url_caches", :force => true do |t|
-    t.string   "url",                            :null => false
-    t.datetime "fetched_at",                     :null => false
-    t.binary   "content",    :limit => 16777216
+    t.string   "url",                              :null => false
+    t.datetime "fetched_at",                       :null => false
+    t.binary   "content",    :limit => 2147483647
   end
 
   create_table "users", :force => true do |t|
